133d58734ba77d6f4e947c00315a0563d654a994
teleanucalin <85694657+teleanucalin@users.noreply.github.com>
2022-04-20 16:55:30 +0300

    Update outputs.tf

--- network/outputs.tf
+++ network/outputs.tf
@@ -3,29 +3,29 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-   value = aws_subnet.public.*.id
+   value = aws_subnet.public[*].id
 }
 
 output "web_subnet_ids" {
-   value = aws_subnet.web.*.id
+   value = aws_subnet.web[*].id
 }
 
 output "app_subnet_ids" {
-   value = aws_subnet.app.*.id
+   value = aws_subnet.app[*].id
 }
 
 output "data_subnet_ids" {
-   value = aws_subnet.data.*.id
+   value = aws_subnet.data[*].id
 }
 
 output "app_subnet_cidr" {
-   value = aws_subnet.app.*.cidr_block
+   value = aws_subnet.app[*].cidr_block
 }
 
 output "web_subnet_cidr" {
-   value = aws_subnet.web.*.cidr_block
+   value = aws_subnet.web[*].cidr_block
 }
 
 output "data_subnet_cidr" {
-   value = aws_subnet.data.*.cidr_block
+   value = aws_subnet.data[*].cidr_block
 }
