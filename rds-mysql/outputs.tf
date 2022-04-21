48e0ae3e7c0425f907c421b78cb16fdb07e32379
teleanucalin <85694657+teleanucalin@users.noreply.github.com>
2022-04-21 14:37:43 +0300

    Update outputs.tf

--- rds-mysql/outputs.tf
+++ rds-mysql/outputs.tf
@@ -1,7 +1,8 @@
-output "core_primary_db_parameter_group" {
+/*output "core_primary_db_parameter_group" {
    value = aws_db_parameter_group.core_primary
 }
 
 output "core_db_subnet_group" {
    value = aws_db_subnet_group.core
 }
+*/
